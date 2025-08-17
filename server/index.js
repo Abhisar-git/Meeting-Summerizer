cc4cc37bde332bda031043439a13d343a46501d4
Abhisar-git <abhisarprincejha@gmail.com>
2025-08-17 18:29:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+app.get('/', (req, res) => {
+  res.send('🚀 Backend is running on Railway!');
+});
+
 // Middleware
 app.use(cors({
   origin: [
